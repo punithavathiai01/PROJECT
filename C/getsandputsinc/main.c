422efe6483afa9d4309eb23c8936e229f921e89b
punithavathiai01 <punithavathid1997@gmail.com>
2024-02-02 11:55:43 +0530

    examplefile

---
+++ C/getsandputsinc/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*int main()
+{
+char name[0];
+
+printf("Enter your name: ");
+gets(name);
+printf("Your name is: ");
+puts(name);
+
+return 0;
+}*/
+
+
+
+   /* int main()
+{
+    int x;
+    char str[100];
+    scanf("%d\n", &x);
+    fgets(str, 100, stdin);
+    printf("x = %d, str = %s", x, str);
+    return 0;
+}*/
