c08608686265606c0da6826e3e7fed446ec03e02
punithavathiai01 <punithavathid1997@gmail.com>
2024-02-09 13:46:29 +0530

    example file of python datatypes

---
+++ python/first.py
@@ -0,0 +1,96 @@
+'''
+print("Hello punitha");
+print("welcome to python");
+txt = "snega is a snake"
+if "snegaa" in txt:
+ print("yes, 'snega' is present") 
+else:
+   print("No, 'snegaa' is not present") 
+a=10;
+b=5;
+'''
+#a=input("Enter value a: ");
+#print(a);
+
+a=90
+b=103
+
+#IF STATEMENT:
+if b > a:
+ print("b is greater than a")
+
+#ELSE:
+if b > a:
+   print ("b is greater than a")
+elif a==b:
+   print ("a and b are equal")
+else:
+   print ("a is greater than b")
+
+
+#ELSE IF STATEMENT:
+if b > a :
+  print("b is greater than a")
+elif a==b:
+  print ("a and b are equal")
+
+#SHORT HAND IF:
+d = 55
+f = 33
+if d>f:print("d is greater than f")
+
+#SHORTHAND IF ELSE:
+print("D") if d > f else print ("F")
+print ("D") if d > f else print("=")if d==f else print ("F")
+
+#NESTED IF:
+if d > 10:
+  print ("Above ten,")
+  if d > 20:
+   print("and also above 20!")
+else:
+  print ("but not above 20.")
+
+
+'''#Loop:
+
+numbers = [4, 2, 6, 7, 3, 5, 8, 10, 6, 1, 9, 2]  
+  
+square = 0  
+  
+
+squares = []  ;
+  
+
+for value in numbers:  
+    square = value ** 2  
+    squares.append(square)  
+print("The list of squares is", squares);  
+
+# WHILELOOP:
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+p = 2
+while p < 6:
+   p += 1
+else:
+   print ("p is no longer less than 6")
+'''
+
+#FORLOOP:
+
+'''colour = ["white","black","red"]
+for x in colour:
+      if x == "black":
+         break
+      print (x)'''
+
+
+
+
+
+
