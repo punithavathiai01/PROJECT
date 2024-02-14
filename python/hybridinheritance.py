52ff0ea9eff58f8c889994ab53631a6713b5693d
punithavathiai01 <punithavathid1997@gmail.com>
2024-02-14 12:30:26 +0530

    added a file of hybrid inheritance with one base class and three child class

---
+++ python/hybridinheritance.py
@@ -0,0 +1,23 @@
+class school:
+    def func(self):
+        print("Welcome To School BRIGHT.")
+
+
+class student1(school):
+    def func1(self):
+        print ("I am a Student In This School My Name is Tamil.")
+
+
+class student2(school):
+    def func2(self):
+        print (" Hi I am a student In This School and My Name is Snega.")
+
+
+class student3(school):
+    def func3(self):
+        print ("Hello I am a Student In This School And My Name Is Srinithi.")
+
+
+object=student1()
+object.func()
+object.func1()
